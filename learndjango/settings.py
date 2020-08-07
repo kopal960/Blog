c8f3365863b89d6fefc2d2e30114fe8ad9481529
kopal960 <kopalmsm@gmail.com>
2020-08-07 13:26:01 +0530

    updated setting

--- learndjango/settings.py
+++ learndjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['blog-project-django.herokuapp.com']
+ALLOWED_HOSTS = ['blog-project-django.herokuapp.com',"127.0.0.1"]
 
 
 # Application definition
